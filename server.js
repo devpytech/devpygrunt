4ee7a65035c5c429b39b280ad59a89f4449454f5
rawas <rawashbourne@gmail.com>
2016-06-23 02:43:04 +0000

    email server, baby! Yeah!

--- server.js
+++ server.js
@@ -7,23 +7,24 @@ var bodyParser = require('body-parser');
 var validator = require('validator');
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))  
-app.use(bodyParser.json())  
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
 app.use(function(req, res, next) {  
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
 app.post('/subscribe', function (req, res) {  
     var email = req.body.email;
-    if(!email || !validator.isEmail(email)) return res.json({success: false, error: "Oops! Incorrect email!"});
+    if(!email || !validator.isEmail(email)) return res.json({success: false, error: "That's not a valid email."});
     mlist.members().create({subscribed: true, address: email}, function (err, data) {
-
-        if(data.message.indexOf("Address already exists") > -1) return res.json({success: false, error: "Oops! This email has already been added!"});
+        if(data.message.indexOf("Address already exists") > -1) return res.json({success: false, error: "You're already subscribed. Thanks!"});
         res.json({success: true});
     });
 });
 
 app.listen(process.env.PORT);  
-console.log("We're listening on port 8080!");
+console.log("listening");
